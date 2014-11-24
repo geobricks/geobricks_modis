0e47f7af1f0629fc0618921f9abb2504b31a114e
Kalimaha <guido.barbaglia@gmail.com>
2014-11-24 13:24:12 +0100

    Config file to be revised.

---
+++ test/downloader_test.py
@@ -0,0 +1,21 @@
+import time
+import sched
+from geobricks_downloader.download.downloader import Downloader
+from geobricks_modis.core.modis_core import list_layers_countries_subset
+
+
+# Filters
+product = 'MOD13A3'
+year = '2001'
+day = '001'
+country = 'IT'
+
+# Get the list of layers through the Geobricks MODIS plug-in
+layers_to_be_downloaded = list_layers_countries_subset(product, year, day, country)
+
+# Target folder: MODIS layers will be downloaded here
+target = {'target': '/home/kalimaha/Desktop/MODIS', 'product': product, 'year': year, 'day': day}
+
+# Run the downloader
+my_downloader = Downloader('modis', target, layers_to_be_downloaded, True)
+downloaded_layers = my_downloader.download()
